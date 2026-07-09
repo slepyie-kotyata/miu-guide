bc17467da7df724a2e406061eec6ab208091826a
sangiriah <lisaosadchenko@gmail.com>
2026-07-09 22:55:26 +0300

    docs: swagger init

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"miu-guide/internal/routes"
-
+	_ "miu-guide/docs"
 	"github.com/labstack/echo/v5"
+	echoSwagger "github.com/swaggo/echo-swagger/v2"
 )
 
+// @title MIU-Guide API
+// @version 1.0
+// @description <Это документация к API MIU-Guide!>
+// @contact.name Команда "Слепые Котята"
+// @contact.url https://github.com/slepyie-kotyata/miu-guide
+// @license.name MIT License
+// @BasePath /
 func main() {
 	e := echo.New()
 
 	routes.InitAuthRoutes(e)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	
 	if err := e.Start(":1323"); err != nil {
     	e.Logger.Error("failed to start server", "error", err)
--- backend/internal/routes/auth.go
+++ backend/internal/routes/auth.go
@@ -6,6 +6,12 @@ import (
 	"github.com/labstack/echo/v5"
 )
 
+// @Summary      Тестовый эндпоинт
+// @Description  Возвращает успешное сообщение для проверки работоспособности API
+// @Tags         test
+// @Produce      json
+// @Success      200  {object}  map[string]string  "Успешное выполнение"
+// @Router       /test [get]
 func InitAuthRoutes(e *echo.Echo) {
 	e.GET("/test", func(c *echo.Context) error {
     	return c.JSON(http.StatusOK, map[string]string{"message": "success"})
