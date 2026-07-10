512fe50c9e3c9c8df2b016a527b2300534df5532
ftorn1k <154966484+ftorn1k@users.noreply.github.com>
2026-07-10 22:54:57 +0300

    feat: основные страницы
    авторизация, карта, расписание

--- frontend/src/app/tab1/tab1.page.ts
+++ frontend/src/app/tab1/tab1.page.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; // 1. Импортируем схему
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  standalone: true,
+  imports: [IonicModule, CommonModule], // CommonModule нужен для *ngFor и [class.active]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // 2. Добавляем эту строчку
 })
+
 export class Tab1Page {
+  // Список этажей (с 6 по 1, как на макете сверху вниз)
+  floors: number[] = [6, 5, 4, 3, 2, 1];
+  
+  // Текущий выбранный этаж
+  currentFloor: number = 1;
+
   constructor() {}
+
+  // Метод для переключения этажа
+  selectFloor(floor: number) {
+    this.currentFloor = floor;
+  }
+
+  // Динамически формируем путь к картинке в зависимости от этажа
+  getMapImage(): string {
+    // Убедись, что в папке assets/maps/ лежат файлы map_1.png, map_2.png и т.д.
+    return `/assets/maps_1/map_${this.currentFloor}.jpg`;
+  }
 }
--- frontend/src/app/tab2/tab2.page.ts
+++ frontend/src/app/tab2/tab2.page.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-
+import { addIcons } from 'ionicons'; 
+import { caretBack, caretForward } from 'ionicons/icons'; 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [ IonContent, IonIcon]
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor() {
+    addIcons({ caretBack, caretForward });
+  }
 
 }
