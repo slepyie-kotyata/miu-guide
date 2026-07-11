29da7f063870213b736cf662091b697c017dfc42
sangiriah <lisaosadchenko@gmail.com>
2026-07-11 04:57:05 +0300

    feat: params and query validation

--- backend/internal/handlers/schedule.go
+++ backend/internal/handlers/schedule.go
@@ -8,6 +8,8 @@ import (
 	"miu-guide/internal/filter"
 	"miu-guide/internal/models"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v5"
 )
@@ -16,21 +18,29 @@ import (
 //TODO: 2) распаковка json (da)
 //TODO: 3) фильтр по disciplineOid, lessonNumberStart (da)
 //TODO: 4) структура ответа (da)
-//TODO: 5) обработка "неправильных" ответов
+//TODO: 5) обработка "неправильных" ответов (da)
 //TODO: 6) работа с redis (если нет -> запрос к апи и кэшируем, если есть -> вытаскиваем)
 //TODO: 7) проверка на формат даты
 
+var layout = "2006.01.02"
+
+func validateDate(date string) bool {
+	_, err := time.Parse(layout, date)
+	return err == nil
+}
+
 func GetSpecificSchedule(c *echo.Context) error {
 	groupId, scheduleDay := c.Param("group"), c.QueryParam("day")
+	if _, err := strconv.Atoi(groupId); err != nil || !validateDate(scheduleDay) {
+		return c.JSON(http.StatusBadRequest, map[string]any{
+			"code": 1,
+		})
+	}
 	client := &http.Client{} // вынести это в структуру
-
-	// тут должна быть проверка на корректность параметров (проверка на число в groupId, проверка на дату scheduleDay)
 
 	urlRequest := fmt.Sprintf("%s/schedule/group/%s?start=%s&finish=%s&lng=1", env.GetEnv(env.ScheduleBaseApiUrl), groupId, scheduleDay, scheduleDay)
     apiReq, _ := http.NewRequest("GET", urlRequest, nil)
     apiReq.SetBasicAuth(env.GetEnv(env.ScheduleUsername), env.GetEnv(env.SchedulePassword))
-
-	fmt.Printf("%s\n", urlRequest) // убрать потом
 
 	apiResp, err := client.Do(apiReq)
 	if err != nil {
@@ -49,6 +59,5 @@ func GetSpecificSchedule(c *echo.Context) error {
 	}
 
 	result := filter.FilterSchedule(schedule)
-	fmt.Println(result)
 	return c.JSON(http.StatusOK, result)
 }
